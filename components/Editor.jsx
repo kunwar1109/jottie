7d346acb6e870ab64b036f022137824d5abd2e34
kunwar1109 <161008465+kunwar1109@users.noreply.github.com>
2024-07-16 12:25:30 +0530

    temp fix for numbered list null bug from blocknote used a temp fix given in github

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -5,6 +5,7 @@ import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useTheme } from "next-themes";
 import { useEdgeStore } from "@/lib/edgestore";
+import { useEffect } from "react";
 
 const Editor = ({ onChange, initialContent, editable }) => {
   const { resolvedTheme } = useTheme();
@@ -22,13 +23,28 @@ const Editor = ({ onChange, initialContent, editable }) => {
     initialContent: initialContent ? JSON.parse(initialContent) : undefined,
     uploadFile: handleUpload,
   });
+
+  useEffect(() => {
+    let timeout;
+    if (editor) {
+      timeout = setTimeout(() => {
+        const firstBlock = editor.document[0];
+        if (firstBlock)
+          editor.updateBlock(firstBlock, { type: firstBlock.type });
+      }, 100);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [editor]);
   return (
     <div>
       <BlockNoteView
         editor={editor}
         editable={editable}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
-        onChange={() => onChange(JSON.stringify(editor.document))}
+        onChange={() => onChange(JSON.stringify(editor.document, null, 2))}
       />
     </div>
   );
